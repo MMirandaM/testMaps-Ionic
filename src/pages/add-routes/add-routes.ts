ad4f0ef206436e9eec79ab62aa6707834632570e
Matheus Matos <m.miranda.m@hotmail.com>
2018-01-09 13:57:15 -0400

    Salvando as rotas no Storage

--- src/pages/add-routes/add-routes.ts
+++ src/pages/add-routes/add-routes.ts
@@ -3,58 +3,86 @@ import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angu
 import { Geolocation } from '@ionic-native/geolocation';
 import { ConfigStorageProvider, Place } from '../../providers/config-storage/config-storage';
 
-/**
- * Generated class for the AddRoutesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
- declare var google;
+declare var google;  // varável global para o Google Maps
 
 @IonicPage()
 @Component({
   selector: 'page-add-routes',
   templateUrl: 'add-routes.html',
 })
+
+
 export class AddRoutesPage {
   @ViewChild('map') mapElement: ElementRef;
   @ViewChild('directionsPanel') directionsPanel: ElementRef;
-  map:any;
+  
+  map:any;  // contém o mapa - Google Maps
 
-  lat:string;
-  lng:string;
+  places:Array<Place> = [];  // contém todos marcadores salvos
+  rota:Array<Place> = [];  // contém os marcadores em uma rota
 
-  places:Array<Place> = [];
-  rota:Array<Place> = [];
+                    // variáveis criadas para se trabalhar dentro do '.ts'
+  date_inicio:any;  // data de inicio, recebe sDate_inicio na forma de 'Date'
+  date_fim:any;     // date de fim, recebe sDate_fim na forma de 'Date'
+  
+                              // variáveis que recebe o valor selecionado no '.html'
+  sDate_inicio:string = "";   // data de inicio
+  sDate_fim:string = "";      // data de fim
 
-  date_inicio:any = "";
-  date_fim:any = "";
-  sDate_inicio:string = "";
-  sDate_fim:string = "";
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation, private alertCtrl: AlertController, public placePvdr: ConfigStorageProvider) {
-    this.lat = this.navParams.get('lat');
-    this.lng = this.navParams.get('lng');
   }
 
+
+  /*
+    Função de inicialização da page
+      
+      -carrega todos os marcadores salvos
+      -chamada da função para apresentação do mapa
+  */
   ionViewDidLoad() {
     this.places = this.placePvdr.getPlaceConfigStorage();
+    
     this.loadMap();
   }
 
+
+  /*
+    Função onde irá carregar o mapa na page
+  */
   loadMap(){
-    let latLng = new google.maps.LatLng(this.lat, this.lng);
-    let mapOptions = {  
-      center: latLng,
-      zoom: 13,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+    this.geolocation.getCurrentPosition().then((position) => {
+
+      // captura a posição atual do usuário
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      
+      // configurações do mapa
+      let mapOption = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      // atribui o mapa para variável
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOption);
+    },
+    (err) => {
+      console.log(err);
     }
-   
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    );
   }
 
-  //Prompt: Alerta de todos os tipos
+
+  /*
+    Função Alert Controller que apresenta uma mensagem
+      na tela de acordo com os parâmetros
+
+      parâmetros:
+        -title: título da mensagem
+        -message: corpo da mensagem
+        -booll: *parâmetro de controle ( casos específicos )
+  */
   Alert(title:string, message:string, booll:boolean){
     let alert = this.alertCtrl.create({
       title: title,
@@ -63,6 +91,9 @@ export class AddRoutesPage {
         text:'Ok'
       }]
     });
+
+    // caso o valor booleano seja 'true'
+    //   as variáveis irão 'reiniciar'
     if(booll == true) {
       this.sDate_inicio = "";
       this.sDate_fim = "";
@@ -71,8 +102,16 @@ export class AddRoutesPage {
     alert.present();
   }
 
+
+  /*
+    Função onde cria a rota de acordo com o intervalo
+      de tempo estabelecido e salva numa variável 'this.rota'
+  */
   createRouter(){
     for (var i in this.places) {
+
+      // captura a string que representa a data e
+      //   transforma-a em um 'Date'
       let date_place = ((this.places[i].date).split(" ")[2]).split("/");
       let date_aux = new Date(Number(date_place[2]), Number(date_place[1]) - 1, Number(date_place[0]));
       
@@ -81,25 +120,36 @@ export class AddRoutesPage {
       }
     }
   }
-
+  
+  
+  /*
+    Função onde carrega a rota no mapa
+  */
   showRouter(){
     this.createRouter();
 
+    // se existe rota no intervalo de tempo
     if (this.rota.length != 0) {
       this.placePvdr.setRouteConfigStorage([this.sDate_inicio + ' à ' + this.sDate_fim ,this.date_inicio, this.date_fim]);
+      
+      // se existe mais de um marcador na rota
       if (this.rota.length > 1) {
+
+        // captura o primeiro marcador
         let place = this.rota.shift();
         let lat = place.latitude;
         let lng = place.longitude;
 
         let origem = {lat:Number(lat), lng:Number(lng)};
 
+        // capura o último marcador
         place = this.rota.pop();
         lat = place.latitude;
         lng = place.longitude;
 
         let fim = {lat:Number(lat), lng:Number(lng)};
 
+        // seleciona os pontos intermediários da rota
         let way_points:Array<any> = [];
         for (var i in this.rota) {
           let lt = this.rota[i].latitude;
@@ -108,11 +158,8 @@ export class AddRoutesPage {
           way_points.push({location:ltLg});
         }
 
-        let renderOptions = {
-          draggable: true
-        };
         let directionsService = new google.maps.DirectionsService;
-        let directionsDisplay = new google.maps.DirectionsRenderer(renderOptions);
+        let directionsDisplay = new google.maps.DirectionsRenderer;
         
         directionsDisplay.setMap(this.map);
         //directionsDisplay.setPanel(this.directionsPanel.nativeElement);
@@ -132,8 +179,9 @@ export class AddRoutesPage {
 
         this.Alert('Informação','Rota adicionada com sucesso!',false);
       
+      // caso existe apenas um marcador
       } else {
-        this.Alert('Informação', 'Existe apenas um marcador no intervalo de tempo escolido',false)
+        this.Alert('Informação', 'Existe apenas um marcador no intervalo de tempo escolido',false);
         let place = this.rota.pop();
 
         let lat = Number(place.latitude);
@@ -154,21 +202,32 @@ export class AddRoutesPage {
       this.rota = [];
 
     } else {
+      // *caso não exista marcadores no intervalo de tempo
+      //    as variáveis sDate_inicio e sDate_fim serão 'reiniciadas'
       this.Alert('Not Found', 'Nao existem marcadores no intervalo de tempo estabelecido!', true);
     }
   }
   
+  /*
+    Função de validação do intervalo de tempo
+  */
   addRouter(){
     if(this.sDate_inicio != "" && this.sDate_fim != ""){
+
+      // captura a data em forma de string
       let aux_inicio = this.sDate_inicio.split("-");
       let aux_fim = this.sDate_fim.split("-");
 
+      // transforma a data 'string' em data 'Date'
       this.date_inicio = new Date(Number(aux_inicio[0]), Number(aux_inicio[1]) - 1, Number(aux_inicio[2]));
       this.date_fim = new Date(Number(aux_fim[0]), Number(aux_fim[1]) - 1, Number(aux_fim[2]));
       
       if (this.date_fim >= this.date_inicio) {
         this.showRouter();
       } else {
+
+        // *caso os valores sejam inválidos, as variáveis sDate_inicio
+        //    e sDate_fim serão 'reiniciadas'
         this.Alert('Dados Incoerentes', 'Verifique se o intervalo de tempo é válido!', true);
       }
     }else{
